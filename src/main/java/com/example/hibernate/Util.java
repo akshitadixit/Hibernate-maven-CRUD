561a1bf74bdc17ee85d89252d96ea0b9c0cbed17
Akshita Dixit <akshitadixit.hack@gmail.com>
2021-06-17 13:24:52 +0530

    Initial commit

---
+++ src/main/java/com/example/hibernate/Util.java
@@ -0,0 +1,24 @@
+package com.example.hibernate;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Util {
+	
+	private static final SessionFactory sessionFactory;
+	
+	static{
+		try{
+			sessionFactory = new Configuration().configure("../resources/hibernate.cfg.xml").buildSessionFactory();
+
+		}catch (Throwable ex) {
+			System.err.println("Session Factory could not be created." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}	
+	}
+	
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+	
+}
---
+++ src/main/java/com/example/hibernate/model/College.java
@@ -0,0 +1,137 @@
+package com.example.hibernate.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "COLLEGE")
+public class College implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "NAME", nullable = false)
+	private String name;
+
+	@Column(name = "STATE", nullable = false)
+	private String state;
+
+    @Column(name = "CITY", nullable = false)
+	private String city;
+
+	@Column(name = "STRENGTH", nullable = false)
+	private int strength;
+
+    @Column(name = "RATING", nullable = false)
+	private double rating;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((city == null) ? 0 : city.hashCode());
+        result = prime * result + id;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(rating);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        result = prime * result + ((state == null) ? 0 : state.hashCode());
+        result = prime * result + strength;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        College other = (College) obj;
+        if (city == null) {
+            if (other.city != null)
+                return false;
+        } else if (!city.equals(other.city))
+            return false;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (Double.doubleToLongBits(rating) != Double.doubleToLongBits(other.rating))
+            return false;
+        if (state == null) {
+            if (other.state != null)
+                return false;
+        } else if (!state.equals(other.state))
+            return false;
+        if (strength != other.strength)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "College [city=" + city + ", id=" + id + ", name=" + name + ", rating=" + rating + ", state=" + state
+                + ", strength=" + strength + "]";
+    }
+
+}
